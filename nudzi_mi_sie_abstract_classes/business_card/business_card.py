b0eacfce948757aab7c6990e533de7877852bbf8
Niemaly <jac3k2@gmail.com>
2020-06-09 17:10:58 +0200

    probem -> i dont want to show field no_want_this. Solution -> just make function that add only fields i need

--- nudzi_mi_sie_abstract_classes/business_card/business_card.py
+++ nudzi_mi_sie_abstract_classes/business_card/business_card.py
@@ -7,9 +7,12 @@ class BusinessCard:
         self.email = f'{self.name}.{self.surname}@{domain}'
         self.no_want_this = "i dont want this in my json"
 
+    def to_jason(self):
+        return {'name': self.name, "surname": self.surname, "email": self.email}
+
 my_business_card = BusinessCard( "Jacek","Szumski")
 
 def to_json(obj):
-    return json.dumps(obj.__dict__)
+    return json.dumps(obj.to_jason())
 
 print(to_json(my_business_card))
